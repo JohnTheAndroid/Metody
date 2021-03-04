615a3800bd1eb496e873efb37caa8cf612ea50dd
Jan <79105540+JohnTheAndroid@users.noreply.github.com>
2021-03-04 12:21:47 +0100

    Add files via upload

---
+++ Metody03/Metody03/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Metody03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private double Vzdalenost(double a1,double a2,double b1, double b2)
+        {
+            double vysledek = Math.Sqrt((a2 - a1) * (a2 - a1) + (b2 - b1) * (b2 - b1));
+            return vysledek;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x1 = double.Parse(textBox1.Text);
+            double x2 = double.Parse(textBox2.Text);
+            double y1 = double.Parse(textBox3.Text);
+            double y2 = double.Parse(textBox4.Text);
+
+            double vysledek = Vzdalenost(x1, x2, y1, y2);
+            MessageBox.Show("Vzdalenost dvou bodů je: " + vysledek);
+        }
+    }
+}
